98c2e7dea75fbf27984cfde83408ee094b81899e
aryanGoel <agoel2992@gmail.com>
2025-10-17 23:58:47 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+'use client' // Required for client-side features like useState, useEffect
+
+import Dashboard from '@/components/Dashboard'
+
+export default function Home() {
+  return <Dashboard />
+}
